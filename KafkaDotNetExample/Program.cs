d55c1dedf69733437950a623a4d456366bb2a04d
Ebrahim Naderloo <e.naderloo@gmail.com>
2024-05-27 18:18:44 +0330

    Add project files.

---
+++ KafkaDotNetExample/Program.cs
@@ -0,0 +1,32 @@
+
+
+using KafkaDotNetExample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ProducerService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/event-producing", async (ProducerService producer, CancellationToken cancellationToken) =>
+{
+
+    await producer.ProduceAsync(cancellationToken);
+
+    return "Event Sent!";
+})
+.WithOpenApi();
+
+app.Run();
